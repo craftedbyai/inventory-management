ae2df63eef3f17a4ef408f954fc19c808195701c
asadsid004 <asadsid004@gmail.com>
2025-02-27 09:40:58 +0530

    chore: image url fix

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -82,7 +82,7 @@ const Sidebar = () => {
         }`}
       >
         <Image
-          src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
+          src="https://s3-inventory-management-aws.s3.us-east-1.amazonaws.com/logo.png"
           alt="Invento-logo"
           width={27}
           height={27}
